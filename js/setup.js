bb20249fdf22bfe0895ab0deb98f8d0dce3ddf9d
Dominik Richter <dominik.richter@gmail.com>
2017-12-06 15:23:29 +0100

    oh-my-zsh

--- js/setup.js
+++ js/setup.js
@@ -18,6 +18,13 @@ const run = x => {
   return shell.exec(x)
 }
 const commandExists = x => spawnSync('command',['-v', x]).status == 0
+
+const gitClone = (src, dst) => run(`git clone ${src} ${dst}`)
+const gitEnsure = (src, path) => {
+  if(!fs.existsSync(path)) gitClone(src, path);
+  return run(`cd ${path} && git fetch --all && git pull`)
+}
+
 const brewInstall = x => {
   res = run(`brew install ${x}`)
   if(res.status == 0) {
@@ -27,6 +34,14 @@ const brewInstall = x => {
   console.log(colorErr(`Failed to install ${x}: ${res.stderr}`));
   return false;
 }
+const isOsx = /^darwin/.test(process.platform)
+const cannotInstall = x =>
+  console.log(colorErr(`Cannot detect OS to install ${x}`)) &&
+  shell.exit(1)
+const packageInstall = x =>
+  isOsx ? brewInstall(x) :
+  cannotInstall(x)
+
 const install = (what, exists, install) => {
   if(exists()) return console.log(colorOk(`[ok] ${what}`));
   return install()
@@ -51,9 +66,23 @@ const syncFiles = (src, dst) => {
 const installBrew = () => run('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 install('brew', () => commandExists('brew'), installBrew)
 
+// AG
+const installAg = () => packageInstall('ag')
+install('ag', () => commandExists('ag'), installAg)
+
 // ZSH
-const installZsh = x => brewInstall('zsh')
+const zshrc = path.join(os.homedir(), '.zshrc')
+const installZsh = () => packageInstall('zsh')
 install('zsh', () => commandExists('zsh'), installZsh)
+const defaultZsh = () => run('chsh -s /bin/zsh')
+install('zsh-default', () => process.env.SHELL == '/bin/zsh', defaultZsh)
+const installOhMyZsh = () => {
+  gitEnsure('git://github.com/robbyrussell/oh-my-zsh.git', path.join(os.homedir(), '.oh-my-zsh'))
+  if(!fs.existsSync(zshrc))
+    shell.cp(`~/.oh-my-zsh/templates/zshrc.zsh-template ${zshrc}`)
+  return run('curl https://raw.githubusercontent.com/arlimus/zero.zsh/master/bootstrap.sh | sh -')
+}
+install('oh-my-zsh', () => false, installOhMyZsh)
 
 // Gitconfig
 const gitconfPath = path.join(os.homedir(), '.gitconfig')
