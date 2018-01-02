66066e684d3cfb2f32025e95d0e40991f2e89609
Dominik Richter <dominik.richter@gmail.com>
2018-01-01 21:48:46 -0800

    provide home env to sudo docker/travis install

--- arch.bootstrap.sh
+++ arch.bootstrap.sh
@@ -10,6 +10,5 @@ sudo -l -U "$username" | grep "not allowed to run sudo" && echo "$username ALL=(
 command -v git || pacman -S git --noconfirm
 rm -rf /tmp/setup && sudo -u "$username" git clone https://github.com/arlimus/setup /tmp/setup
 
-set +x
-GIT_USER_NAME="My Name" GIT_USER_EMAIL="my-e@mail.com" \
+HOME="/home/$username" GIT_USER_NAME="My Name" GIT_USER_EMAIL="my-e@mail.com" \
   sudo -E -u "$username" -- bash /tmp/setup/arch
