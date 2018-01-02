ccec8c2d13f0962526246168c2c659c238052be7
Dominik Richter <dominik.richter@gmail.com>
2018-01-01 22:33:45 -0800

    make sure travis runs from latest folder
    dont clone doh

--- arch.bootstrap.sh
+++ arch.bootstrap.sh
@@ -8,7 +8,8 @@ pacman -Sy
 command -v sudo || pacman -S sudo --noconfirm
 sudo -l -U "$username" | grep "not allowed to run sudo" && echo "$username ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 command -v git || pacman -S git --noconfirm
-rm -rf /tmp/setup && sudo -u "$username" git clone https://github.com/arlimus/setup /tmp/setup
+test ! -d /tmp/setup && git clone https://github.com/arlimus/setup /tmp/setup
+chown -R "$username":"$username" /tmp/setup
 
 HOME="/home/$username" GIT_USER_NAME="My Name" GIT_USER_EMAIL="my-e@mail.com" INSTALL_EXTRAS="false" \
   sudo -E -u "$username" -- bash /tmp/setup/arch
