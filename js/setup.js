bc71cdf1760150187ae0b8199398b2712dd537d3
Dominik Richter <dominik.richter@gmail.com>
2018-01-28 17:31:01 -0800

    httpie

--- js/setup.js
+++ js/setup.js
@@ -172,7 +172,7 @@ function installArchPackages() {
     // devices
     'android-tools',
     // productivity
-    'meld', 'colordiff', 'docker', 'gimp', 'inotify-tools',
+    'meld', 'colordiff', 'docker', 'gimp', 'inotify-tools', 'httpie',
     // unproductivity
     'youtube-dl', 'telegram-desktop-bin', 'slack-desktop', 'mpv', 'x265', 'alsa-utils',
     'gthumb', 'gnome-screenshot', 'evince', 'mediainfo', 'vorbis-tools', 'opus-tools',
