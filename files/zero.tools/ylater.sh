622a49accf6eee2c93cea0f0ac6ea2d8693f7379
Dominik Richter <dominik.richter@gmail.com>
2019-08-20 12:16:05 -0700

    ylater added, opinionated script helper
    not installed by default

---
+++ files/zero.tools/ylater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+args=("$@")
+
+ylater() {
+  url="$1"
+  test -z "$url" && echo "need a youtube link" && exit 1
+  f="$(youtube-dl --get-filename "$url")"
+  d="$(date +"%m-%d")"
+  echo -e "Download \e[35m$url\e[0m to\n\e[36m$f\e[0m, ok?"
+  read -s -n 1 key
+  mkdir -p ~/4serv/"$d"
+  cd ~/4serv/"$d"
+  youtube-dl -f 18 "$url"
+  echo "$url" > "$f".url
+}
+
+ylater "${args[0]}"
