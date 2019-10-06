67d61cc336be51c17e0358f7c16d9ac9d9b0b0a4
Dominik Richter <dominik.richter@gmail.com>
2019-10-05 22:17:00 -0700

    record to mp4 since it is more widely accepted

--- files/reco.sh
+++ files/reco.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# kill any running recordings
+pkill -f "ffmpeg -f x11grab"
+
 mkdir -p ~/recordings
-dst=~/recordings/"$(date "+%Y.%m.%d-%H.%M.%S").mkv"
+dst=~/recordings/"$(date "+%Y.%m.%d-%H.%M.%S").mp4"
 
 slop=$(slop -o -f "%x %y %w %h %g %i")
 read -r X Y W H G ID < <(echo $slop)
