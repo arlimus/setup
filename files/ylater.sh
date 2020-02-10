960028d889f53bd012df2d7c48c9fc111ee780d8
Dominik Richter <dominik.richter@gmail.com>
2020-02-10 14:27:53 -0800

    🐛 add missing color on ylater

--- files/ylater.sh
+++ files/ylater.sh
@@ -6,7 +6,7 @@ test -z "$url" && echo "need a youtube link" && exit 1
 f="$(youtube-dl --get-filename "$url")"                                                                                                                      
 d="$(date +"%m-%d")"                                                                                                                                         
 
-echo "Download \e[35m$url\e[0m to\n\e[36m$f\e[0m"                                                                                                            
+echo -e "Download \e[35m$url\e[0m to\n\e[36m$f\e[0m"
 #read -s -n 1 key                                                                                                                                            
 mkdir -p ~/4serv/"$d"                                                                                                                                        
 cd ~/4serv/"$d"                                                                                                                                              
