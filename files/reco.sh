fb14c25e0d4d8ff363a4d19f9d7963cde8349176
Dominik Richter <dominik.richter@gmail.com>
2021-04-07 21:27:21 -0700

    🐛 change recording killer from border-detection to mouse9 button

--- files/reco.sh
+++ files/reco.sh
@@ -3,7 +3,8 @@
 # kill any running recordings
 pkill -f "ffmpeg -f x11grab"
 
-xdotool behave_screen_edge top-right exec sh -c 'pkill -f "ffmpeg -f x11grab"' &
+# TODO: not sure why this is not working consistently in i3 anymore
+# xdotool behave_screen_edge top-right exec sh -c 'pkill -f "ffmpeg -f x11grab"' &
 
 mkdir -p ~/recordings
 dst=~/recordings/"$(date "+%Y.%m.%d-%H.%M.%S").mp4"
@@ -13,6 +14,8 @@ read -r X Y W H G ID < <(echo $slop)
 #ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f alsa -i pulse ~/myfile.webm
 ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -framerate 25 $dst
 
-pkill -f "xdotool behave_screen_edge"
+# TODO: not sure why this is not working consistently in i3 anymore
+# pkill -f "xdotool behave_screen_edge"
+
 echo "Saved to $dst"
 
--- files/recoa.sh
+++ files/recoa.sh
@@ -3,7 +3,8 @@
 # kill any running recordings
 pkill -f "ffmpeg -f x11grab"
 
-xdotool behave_screen_edge top-right exec sh -c 'killall ffmpeg' &
+# TODO: not sure why this is not working consistently in i3 anymore
+#xdotool behave_screen_edge top-right exec sh -c 'killall ffmpeg' &
 
 mkdir -p ~/recordings
 dst=~/recordings/"$(date "+%Y.%m.%d-%H.%M.%S").mp4"
@@ -12,6 +13,8 @@ slop=$(slop -o -f "%x %y %w %h %g %i")
 read -r X Y W H G ID < <(echo $slop)
 ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y -f pulse -i 1 -framerate 25 $dst
 
-pkill -f "xdotool behave_screen_edge"
+# TODO: not sure why this is not working consistently in i3 anymore
+#pkill -f "xdotool behave_screen_edge"
+
 echo "Saved to $dst"
 
