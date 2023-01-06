c1f0905c53ae8f1712c4d1fa1374efabad9b3c40
Dominik Richter <dominik.richter@gmail.com>
2023-01-06 14:44:36 -0800

    🐛 clipboard errors => use tmp file instead

--- files/gsac.sh
+++ files/gsac.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-maim -s | xclip -selection clipboard -t image/png
+
+# Note: 2022-12-25, this method of keeping it all in-memory didn't work
+# on current versions of Arch Linux. The file-based approach continues to
+# work.
+# maim -s | xclip -selection clipboard -t image/png
+
+maim -s | convert - /tmp/gsac.png
+xclip -selection clipboard -t image/png -i /tmp/gsac.png
