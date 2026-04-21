b0638dcc4c72429ab006940fa9ee1ade746ede45
Dominik Richter <dominik.richter@gmail.com>
2026-04-20 18:00:51 -0700

    🐛 fix all screenshot runners gsac/gsa

--- files/gsa.sh
+++ files/gsa.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-maim -u -s ~/"Screenshots/$(date "+%Y.%m.%d-%H:%M:%S").png"
-
-# alternative using gnome-screenshot
-#gnome-screenshot -a
+if [ "$XDG_SESSION_TYPE" = "wayland" ] || [ -n "$WAYLAND_DISPLAY" ]; then
+    grim -g "$(slurp)" ~/"Screenshots/$(date "+%Y.%m.%d-%H:%M:%S").png"
+else
+    #gnome-screenshot -a
+    maim -u -s ~/"Screenshots/$(date "+%Y.%m.%d-%H:%M:%S").png"
+fi
